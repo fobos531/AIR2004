2ecf7ebf2ec71f223e9bcaa4fa7e57eb3a8e41bc
Jakov Glavina <cyber.shot.jakov@gmail.com>
2020-11-04 22:59:57 +0100

    implement endpoint for getting all users

--- server/models/user.js
+++ server/models/user.js
@@ -28,4 +28,13 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
 module.exports = mongoose.model("User", userSchema);
