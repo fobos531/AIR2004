9e7a80ca59193898057cab3a47821934ae155e3b
fifi98 <bel.filip@hotmail.com>
2020-11-13 19:24:36 +0100

    Mobile - added axios instance

--- mobile/screens/Login.js
+++ mobile/screens/Login.js
@@ -7,21 +7,26 @@ import {
   TouchableOpacity,
   Keyboard,
   Image,
-  ActivityIndicator
+  ActivityIndicator,
 } from 'react-native';
 
-import {Dialog, Portal, TextInput, Button, Provider as PaperProvider} from 'react-native-paper';
+import {
+  Dialog,
+  Portal,
+  TextInput,
+  Button,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import api from '../utils/api';
 
-import Registration from './Registration';
+const Login = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [emailChangePassword, setEmailChangePassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [showHidePassword, setShowHidePassword] = useState(false);
 
-const Login = ({ navigation }) => {
-  const[email, setEmail] = useState('');
-  const[emailChangePassword, setEmailChangePassword] = useState('');
-  const[password, setPassword] = useState('');
-  const[showHidePassword, setShowHidePassword] = useState(false);
-  
-  const[showLoadingIndicator, setShowLoadingIndicator] = useState(false);
-  const[visible, toggleVisible] = useState(false);
+  const [showLoadingIndicator, setShowLoadingIndicator] = useState(false);
+  const [visible, toggleVisible] = useState(false);
 
   const handleShowHidePassword = () => {
     setShowHidePassword(!showHidePassword);
@@ -29,6 +34,14 @@ const Login = ({ navigation }) => {
 
   const handleLoginRequest = () => {
     //TO DO -> spajanje na backend
+    api
+      .post('/user/login', {email, password})
+      .then(({data}) => {
+        console.log(data.user.token);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     console.log('Sending request for login...');
   };
 
@@ -42,86 +55,110 @@ const Login = ({ navigation }) => {
 
     //TO DO -> spajanje na backend
     console.log('Sending request for changing password...');
-  }
+  };
 
   return (
     <PaperProvider>
-        <Portal>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View style={styles.container}>
-              <View>
-                <Image
-                  style={styles.logo}
-                  source={require('../assets/logo.png')}
-                />
-              </View>
-
-              <View style={{marginTop: 60}}>
-                <TextInput
-                  style={styles.textInput}
-                  label="E-mail"
-                  value={email}
-                  mode="outlined"
-                  onChangeText={(email) => setEmail(email)}
-                />
-
-                <TextInput
-                  style={styles.textInput}
-                  secureTextEntry={showHidePassword === true ? false : true}
-                  label="Password"
-                  value={password}
-                  mode="outlined"
-                  onChangeText={(password) => setPassword(password)}
-                  right={
-                    <TextInput.Icon
-                      style={styles.eyeIcon}
-                      name={showHidePassword === true ? 'eye' : 'eye-off'}
-                      onPress={handleShowHidePassword}
-                    />
-                  }
-                />
-              </View>
-
-              <View style={styles.signButton}>
-                <Button
-                  contentStyle={{height: 46}}
-                  mode="contained"
-                  onPress={handleLoginRequest}>
-                  SIGN IN
-                </Button>
-              </View>
-
-              <View style={styles.textContainer}>
-                <TouchableOpacity onPress={() => navigation.push("Registration")}>
-                  <Text>Don't have an account?</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity onPress={() => toggleVisible(true)}>
-                  <Text style={{marginTop: 50}}>Forgot password?</Text>
-                </TouchableOpacity>
-
-                <Portal>
-                  <Dialog visible={visible} onDismiss={() => {toggleVisible(false); setEmailChangePassword('');}}>
-                  <View style={{flexDirection: "row", justifyContent: "space-between", marginRight: 20}}><Dialog.Title>Change password</Dialog.Title>{showLoadingIndicator && <ActivityIndicator size="large" color="#0000ff" />}</View>
-                    <Dialog.Content>
-                      <TextInput
-                        label="Enter your E-mail"
-                        value={emailChangePassword}
-                        mode= "outlined"
-                        onChangeText={(emailChangePassword) => setEmailChangePassword(emailChangePassword)}
-                      />
-
-                    </Dialog.Content>
-                    <Dialog.Actions>
-                      <Button onPress={() => {toggleVisible(false); setEmailChangePassword('');}}>Close</Button>
-                      <Button onPress={() => handleSubmitChangePassword()}>Reset Password</Button>
-                    </Dialog.Actions>
-                  </Dialog>
-                </Portal>
-              </View>
+      <Portal>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.container}>
+            <View>
+              <Image
+                style={styles.logo}
+                source={require('../assets/logo.png')}
+              />
             </View>
-          </TouchableWithoutFeedback>
-        </Portal>
+
+            <View style={{marginTop: 60}}>
+              <TextInput
+                style={styles.textInput}
+                label="E-mail"
+                value={email}
+                mode="outlined"
+                onChangeText={(email) => setEmail(email)}
+              />
+
+              <TextInput
+                style={styles.textInput}
+                secureTextEntry={showHidePassword === true ? false : true}
+                label="Password"
+                value={password}
+                mode="outlined"
+                onChangeText={(password) => setPassword(password)}
+                right={
+                  <TextInput.Icon
+                    style={styles.eyeIcon}
+                    name={showHidePassword === true ? 'eye' : 'eye-off'}
+                    onPress={handleShowHidePassword}
+                  />
+                }
+              />
+            </View>
+
+            <View style={styles.signButton}>
+              <Button
+                contentStyle={{height: 46}}
+                mode="contained"
+                onPress={handleLoginRequest}>
+                SIGN IN
+              </Button>
+            </View>
+
+            <View style={styles.textContainer}>
+              <TouchableOpacity onPress={() => navigation.push('Registration')}>
+                <Text>Don't have an account?</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => toggleVisible(true)}>
+                <Text style={{marginTop: 50}}>Forgot password?</Text>
+              </TouchableOpacity>
+
+              <Portal>
+                <Dialog
+                  visible={visible}
+                  onDismiss={() => {
+                    toggleVisible(false);
+                    setEmailChangePassword('');
+                  }}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      justifyContent: 'space-between',
+                      marginRight: 20,
+                    }}>
+                    <Dialog.Title>Change password</Dialog.Title>
+                    {showLoadingIndicator && (
+                      <ActivityIndicator size="large" color="#0000ff" />
+                    )}
+                  </View>
+                  <Dialog.Content>
+                    <TextInput
+                      label="Enter your E-mail"
+                      value={emailChangePassword}
+                      mode="outlined"
+                      onChangeText={(emailChangePassword) =>
+                        setEmailChangePassword(emailChangePassword)
+                      }
+                    />
+                  </Dialog.Content>
+                  <Dialog.Actions>
+                    <Button
+                      onPress={() => {
+                        toggleVisible(false);
+                        setEmailChangePassword('');
+                      }}>
+                      Close
+                    </Button>
+                    <Button onPress={() => handleSubmitChangePassword()}>
+                      Reset Password
+                    </Button>
+                  </Dialog.Actions>
+                </Dialog>
+              </Portal>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Portal>
     </PaperProvider>
   );
 };
