f68f324fec5f83b7f07af2e51f24190b5346bf84
Jakov Glavina <cyber.shot.jakov@gmail.com>
2020-11-14 15:58:44 +0100

    Fix private route

--- web/src/components/PrivateRoute.js
+++ web/src/components/PrivateRoute.js
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 import api from "../api/api";
 
-const reqConfig = {
-  headers: {
-    Authorization: `Bearer ${localStorage.getItem("userToken")}`,
-  },
-};
-
 const PrivateRoute = ({ component: Component, ...rest }) => {
+  const loggedUser = useSelector((state) => state.loggedUser);
+  const reqConfig = {
+    headers: {
+      Authorization: `Bearer ${loggedUser ? loggedUser.token : null}`,
+    },
+  };
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isBusy, setIsBusy] = useState(true);
   useEffect(() => {
-    console.log("REQ CONFIG", reqConfig);
-    api.get("/user/verify", reqConfig).then((response) => {
-      if (response.data.success == true) {
-        setIsAuthenticated(true);
-      }
-      setIsBusy(false);
-    });
+    console.log("TOKEN", localStorage.getItem("userToken"));
+    api
+      .get("/user/verify", reqConfig)
+      .then((response) => {
+        if (response.data.success == true) {
+          setIsAuthenticated(true);
+        }
+      })
+      .finally(() => {
+        setIsBusy(false);
+      });
   }, []); // do this only once
   if (isBusy) return null;
   if (isAuthenticated)
