77279ae31cff8514fc0061330c87eb26226e4ec9
fifi98 <bel.filip@hotmail.com>
2020-11-21 17:25:49 +0100

    Backend - make socket.io instance global

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -1,16 +1,13 @@
 const crypto = require("crypto");
 
 const websocket = (server) => {
-  const io = require("socket.io")(server);
-
+  global.io = require("socket.io")(server);
   console.log("WebSocket server started!");
 
-  // Generate random token and send it back
-  io.on("connect", (socket) => {
-    console.log("Connected");
-
-    const token = crypto.randomBytes(64).toString("hex");
-    socket.emit("tokenReceived", { token });
+  // When tablet app connects, generate random token and send it back
+  global.io.on("connect", (socket) => {
+    socket.data = { token: crypto.randomBytes(64).toString("hex") };
+    socket.emit("tokenReceived", { token: socket.data.token });
   });
 };
 
