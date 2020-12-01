788c48c1272496aee98db02eff5757d89ebe5af2
pgalina1 <pgalina@foi.hr>
2020-12-01 17:29:29 +0100

    Implemented Dashboard after login

--- mobile/navigation/Teacher.js
+++ mobile/navigation/Teacher.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { View } from "react-native"
 import { createStackNavigator } from "@react-navigation/stack";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import FontAwesomeIcons from "react-native-vector-icons/FontAwesome5";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"
 
 import Dashboard from "../screens/teacher/Dashboard";
 import Attendance from "../screens/teacher/Attendance";
 import Courses from "../screens/teacher/Courses";
 import QR from "../screens/teacher/QR";
-import { Title } from "react-native-paper";
+
+import { signOut } from "../actions";
 
 const Stack = createStackNavigator();
 const Tabs = createMaterialBottomTabNavigator();
@@ -69,6 +73,8 @@ function getHeaderTitle(route) {
 }
 
 const Teacher = () => {
+  const dispatch = useDispatch();
+
   return(
     <Stack.Navigator>
       <Stack.Screen 
@@ -77,7 +83,12 @@ const Teacher = () => {
         options={({ route }) => ({
           headerTitle: getHeaderTitle(route),
           headerTintColor: 'white', 
-          headerStyle: { backgroundColor: '#6202EE' }
+          headerStyle: { backgroundColor: '#6202EE' },
+          headerRight: () => (
+            <View style={{marginRight: 15}}>
+              <MaterialIcons name="logout" size={34} color={"white"} onPress={() => dispatch(signOut())}/>
+            </View>
+          )
         })}
       />
 
--- mobile/screens/teacher/Dashboard.js
+++ mobile/screens/teacher/Dashboard.js
@@ -1,19 +1,105 @@
-import React from "react";
-import { View } from "react-native";
-import { Button } from "react-native-paper";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, StyleSheet } from "react-native";
+import { Button, Text, FAB, Provider as PaperProvider } from "react-native-paper";
+import { useSelector, useDispatch } from "react-redux";
 
-import { signOut } from "../../actions";
+import { signIn } from "../../actions";
+
+const DashboardAfterLogin = () => {
+  const user = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    checkIfSignedIn();
+  }, []);
+
+  const checkIfSignedIn = async () => {
+    const userData = await AsyncStorage.getItem("user");
+    if (userData) dispatch(signIn(JSON.parse(userData)));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hi, <Text style={{ fontWeight: "bold" }}>{user.name} {user.surname}!</Text></Text>
+
+      <View style={{marginTop: 25}}>
+
+        <Text style={styles.font}>You are currently not signed in a lecture room.</Text>
+
+        <View style={{marginTop: 25}}>
+
+          <View style={styles.stepContainer}>
+            <Text style={{fontWeight:"bold", fontSize: 20}}>Step 1</Text>
+            <Text style={{...styles.font, marginTop: 10, marginBottom: 10}}>Go to a lecture room.</Text>
+          </View>
+
+          <View style={styles.stepContainer}>
+            <Text style={{fontWeight:"bold", fontSize: 20}}>Step 2</Text>
+            <Text style={{...styles.font, marginTop: 10, marginBottom: 10}}>Click "Sign in on tablet" button on this screen.</Text>
+          </View>
+
+          <View style={styles.stepContainer}>
+            <Text style={{fontWeight:"bold", fontSize: 20}}>Step 3</Text>
+            <Text style={{...styles.font, marginTop: 10, marginBottom: 10}}>Scan the QR code that's displayed on the tablet in the lecture room.</Text>
+          </View>
+
+        </View>
+      </View>
+    </View>
+  )
+}
 
 const Dashboard = ({ navigation }) => {
   const dispatch = useDispatch();
 
   return (
-    <View>
-      <Button onPress={() => dispatch(signOut())}>Sign out</Button>
-      <Button onPress={() => navigation.push("QRScan")}>Tablet login</Button>
-    </View>
+    <PaperProvider>
+      
+      <DashboardAfterLogin/>
+
+      <FAB
+        style={styles.fab}
+        small
+        label="SIGN IN ON TABLET"
+        icon="qrcode"
+        color="black"
+        onPress={() => console.log('Pressed')}
+        onPress={() => navigation.push("QRScan")}
+      />
+
+      {/*<Button onPress={() => dispatch(signOut())}>Sign out</Button>
+      <Button onPress={() => navigation.push("QRScan")}>Tablet login</Button>*/}
+    </PaperProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    margin: 25
+  },
+
+  font: {
+    fontSize: 15
+  },
+
+  title: {
+    fontSize: 24
+  },
+
+  stepContainer: {
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderColor: "#bbbfc4"
+  },
+
+  fab: {
+    position: 'absolute',
+    marginBottom: 40,
+    marginRight: 20,
+    right: 0,
+    bottom: 0
+  }
+});
 
 export default Dashboard;
