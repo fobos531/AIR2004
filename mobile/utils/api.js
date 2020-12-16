2b9da53bd8b1e09ff98ef8194d673098b5022526
Jakov Glavina <cyber.shot.jakov@gmail.com>
2020-12-16 20:06:22 +0100

    Reset password - frontend part

--- mobile/utils/api.js
+++ mobile/utils/api.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import {API_URL} from '@env';
+import axios from "axios";
+import { API_URL } from "@env";
 
 export default axios.create({
-  baseURL: "http://192.168.1.9:8080/api",
+  baseURL: "http://192.168.1.5:8080/api",
 });
