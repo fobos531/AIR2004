79715740632fc7717416994cf16c83b2ced459ac
pgalina1 <pgalina@foi.hr>
2020-12-17 17:16:31 +0100

    Preparing QR component for connecting with API

--- mobile/screens/student/QR.js
+++ mobile/screens/student/QR.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import QRCodeScanner from "react-native-qrcode-scanner";
 import { Dimensions, View } from "react-native";
+import { Text } from 'react-native-paper';
 
 import api from "../../utils/api";
 
 const QR = () => {
+  const[attendanceSubmited, setAttendanceSubmited] = useState(null);
+
   const onScanned = (e) => {
-    console.log(e.data);
+    const scannedData = (e.data);
+    console.log(scannedData);
 
     //TODO -> spremanje u bazu
+
+    /*Na temelju dobivenog responsea od API-a provjeriti da li je prisustvo uspješno evidentirano*/  
+    
+    /*Ako je uspješno evidentirano -> postavi attendanceSubmited na true*/
+    
   };
 
   return (
     <View>
-      <QRCodeScanner onRead={onScanned} showMarker={true} cameraStyle={{ height: Dimensions.get("window").height }} />
+      {attendanceSubmited === null ? (
+        <QRCodeScanner onRead={onScanned} showMarker={true} cameraStyle={{ height: Dimensions.get("window").height }} />
+      ) : attendanceSubmited === true ? (
+        <Text>Evidencija prisustva uspješna!</Text>
+      ) : (
+        <Text>Evidencija prisustva neuspješna!</Text>
+      )}
+      
     </View>
   );
 };
