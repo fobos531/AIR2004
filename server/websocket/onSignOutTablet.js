debd48587f716837c9da33ae1909e35cac27ed35
fifi98 <bel.filip@hotmail.com>
2020-12-21 18:33:32 +0100

    Tablet - added scan success animation

--- server/websocket/onSignOutTablet.js
+++ server/websocket/onSignOutTablet.js
@@ -1,7 +1,10 @@
 const onSignOutTablet = ({ token }) => {
   let tabletSocket;
   for (let socket of global.io.of("/").sockets.values()) if (socket.data && socket.data.token == token) tabletSocket = socket;
-  if (tabletSocket) tabletSocket.emit("signOutTablet");
+  if (tabletSocket) {
+    tabletSocket.codes = null;
+    tabletSocket.emit("signOutTablet");
+  }
 };
 
 module.exports = onSignOutTablet;
--- tablet/screens/LectureInProgress.js
+++ tablet/screens/LectureInProgress.js
@@ -4,12 +4,15 @@ import { View, StyleSheet, Dimensions, Text } from "react-native";
 import { useSelector } from "react-redux";
 import { Headline } from "react-native-paper";
 import QRCode from "react-native-qrcode-svg";
+import AnimatedCheckmark from "../components/AnimatedCheckmark";
 
 const LectureInProgress = ({ courseName, lectureType, socket, tabletToken }) => {
   const user = useSelector((state) => state);
 
   const [tracking, setTracking] = useState(false);
+  const [code, setCode] = useState(null);
   const [lecture, setLecture] = useState(null);
+  const [successfulScan, setSuccessfulScan] = useState(false);
 
   useEffect(() => {
     socket.on("startTrackingAttendance", () => {
@@ -27,13 +30,16 @@ const LectureInProgress = ({ courseName, lectureType, socket, tabletToken }) => 
     // The new QR code generated and sent by the server
     socket.on("attendanceCode", ({ code }) => {
       console.log("CODE", code);
-      setTracking(code);
+      setSuccessfulScan(false);
+      setCode(code);
+      setTracking(true);
     });
 
     // When the QR code has been successfuly scanned, send message to the server to generate a new one
     socket.on("scanSucess", (data) => {
-      console.log("SUCCESSSSS", data);
-      socket.emit("generateQR", { lectureId: "5fc271812380d73fb1423d1d", token: tabletToken.token });
+      setCode(null);
+      setSuccessfulScan(true);
+      setTimeout(() => socket.emit("generateQR", { lectureId: "5fc271812380d73fb1423d1d", token: tabletToken.token }), 2000);
     });
   }, []);
 
@@ -41,7 +47,8 @@ const LectureInProgress = ({ courseName, lectureType, socket, tabletToken }) => 
     return (
       <View style={styles.qrContainer}>
         <Text style={styles.text}>Please scan the QR code using Unittend application to mark your attendance</Text>
-        <QRCode value={tracking} style={styles.qr} size={Dimensions.get("screen").height * 0.45} />
+        {successfulScan && <AnimatedCheckmark />}
+        {code && <QRCode value={code} style={styles.qr} size={Dimensions.get("screen").height * 0.45} />}
       </View>
     );
 
