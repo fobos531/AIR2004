6b6a0c6cb00f936d27a4f5d0891dc15e2ec09fa6
fifi98 <bel.filip@hotmail.com>
2020-12-26 15:43:39 +0100

    Teacher dashboard code cleanup

--- mobile/screens/teacher/Dashboard.js
+++ mobile/screens/teacher/Dashboard.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useRef } from "react";
-import { View, StyleSheet } from "react-native";
-import { Text, FAB, Provider as PaperProvider } from "react-native-paper";
 import { useSelector, useDispatch } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import { Text, FAB } from "react-native-paper";
 import { io } from "socket.io-client";
+
 import { setCourseSelectedOnTablet, signOutTablet, startTracking } from "../../actions";
-import DashboardAfterLogin from "./components/DashboardAfterLogin";
-import DashboardAfterTabletLogin from "./components/DashboardAfterTabletLogin";
 import DashboardAfterCourseSelection from "./components/DashboardAfterCourseSelection";
+import DashboardAfterTabletLogin from "./components/DashboardAfterTabletLogin";
+import DashboardBeforeTabletLogin from "./components/DashboardAfterLogin";
 import { WSS_URL } from "../../constants";
 
 const Dashboard = ({ navigation }) => {
@@ -35,24 +36,30 @@ const Dashboard = ({ navigation }) => {
   const handleStartTracking = () => {
     dispatch(startTracking());
     socket.current.emit("start tracking", { lecture: user.courseSelectedOnTablet.lecture.id });
+    navigation.navigate("Attendance");
+  };
+
+  const DashBoardContainer = () => {
+    // Dashboard before teacher signs in on the tablet
+    if (!user.attendanceToken) return <DashboardBeforeTabletLogin />;
+
+    // Dashboard after teacher has signed in on the tablet
+    if (!user.courseSelectedOnTablet) return <DashboardAfterTabletLogin handleSignOut={handleSignOut} />;
+
+    // Dashboard after course is selected on the tablet
+    return <DashboardAfterCourseSelection handleSignOut={handleSignOut} handleStartTracking={handleStartTracking} />;
   };
 
   return (
     <View>
       <Text style={styles.title}>
         Hi,{" "}
-        <Text style={{ fontWeight: "bold" }}>
+        <Text style={styles.textBold}>
           {user.name} {user.surname}!
         </Text>
       </Text>
       <View style={styles.container}>
-        {user.attendanceToken == null ? (
-          <DashboardAfterLogin />
-        ) : user.courseSelectedOnTablet == null ? (
-          <DashboardAfterTabletLogin handleSignOut={handleSignOut} />
-        ) : (
-          <DashboardAfterCourseSelection handleSignOut={handleSignOut} handleStartTracking={handleStartTracking} />
-        )}
+        <DashBoardContainer />
         {user.attendanceToken == null && (
           <FAB style={styles.fab} small label="SIGN IN ON TABLET" icon="qrcode" color="black" onPress={() => navigation.push("QRScan")} />
         )}
@@ -70,20 +77,20 @@ const styles = StyleSheet.create({
   font: {
     fontSize: 15,
   },
-
   title: {
     fontSize: 24,
     paddingTop: "3.75%",
     paddingLeft: "4%",
     paddingRight: "4%",
   },
-
+  textBold: {
+    fontWeight: "bold",
+  },
   stepContainer: {
     marginTop: 10,
     borderBottomWidth: 1,
     borderColor: "#bbbfc4",
   },
-
   fab: {
     position: "absolute",
     marginBottom: 110,
