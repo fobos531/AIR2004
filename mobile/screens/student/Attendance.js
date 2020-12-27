cf3c28619a53bba88b799bf60e616d75d4f44eca
pgalina1 <pgalina@foi.hr>
2020-12-27 11:20:32 +0100

    Fetching data from endpoint on attendance screen

--- mobile/screens/student/Attendance.js
+++ mobile/screens/student/Attendance.js
@@ -1,87 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
 import { Text } from "react-native-paper";
-import {Picker} from '@react-native-picker/picker';
+import { Picker } from "@react-native-picker/picker";
+import { useSelector } from "react-redux";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import AttendanceItem from "../student/components/AttendanceItem";
 
-const Attendance = () => {
-  const[selectedFilter, setSelectedFilter] = useState("Courses");
+import api from "../../utils/api";
 
-  const mockData=[
-    {
-      id: '1',
-      date: '20',
-      month: 'Oct',
-      day: 'MONDAY',
-      courseName: 'Foreign Trade',
-      attendanceTime: '10:01',
-      present: true
-    },
-    {
-      id: '2',
-      date: '10',
-      month: 'Nov',
-      day: 'MONDAY',
-      courseName: 'Software Analysis and Design',
-      attendanceTime: '13:48',
-      present: false
-    },
-    {
-      id: '3',
-      date: '10',
-      month: 'Nov',
-      day: 'MONDAY',
-      courseName: 'Software Analysis and Design',
-      attendanceTime: '13:48',
-      present: false
-    },
-    {
-      id: '4',
-      date: '20',
-      month: 'Oct',
-      day: 'MONDAY',
-      courseName: 'Foreign Trade',
-      attendanceTime: '10:01',
-      present: true
-    },
-    {
-      id: '5',
-      date: '20',
-      month: 'Oct',
-      day: 'MONDAY',
-      courseName: 'Foreign Trade',
-      attendanceTime: '10:01',
-      present: true
-    },
-    {
-      id: '6',
-      date: '10',
-      month: 'Nov',
-      day: 'MONDAY',
-      courseName: 'Software Analysis and Design',
-      attendanceTime: '13:48',
-      present: false
-    }
-  ]; 
+const Attendance = () => {
+  const [selectedFilter, setSelectedFilter] = useState("Courses");
+  const [attendanceData, setAttendanceData] = useState([]);
+
+  const user = useSelector((state) => state);
+
+  useEffect(() => {
+    api
+      .get("/attendance", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then(({ data }) => setAttendanceData(data.data))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
-    <View style={{margin: 12, marginBottom: 60}}>
+    <View style={{ margin: 12, marginBottom: 60 }}>
+      <View style={{ flexDirection: "row" }}>
+        <Text
+          style={{
+            fontSize: 17,
+            marginLeft: 10,
+            marginBottom: 15,
+            marginTop: 5,
+          }}
+        >
+          Filter by:{" "}
+        </Text>
 
-      <View style={{flexDirection: "row"}}>
-        <Text style={{fontSize: 17, marginLeft: 10, marginBottom: 15, marginTop: 5}}>Filter by: </Text>
-        
-        <View style={{marginLeft: 10, marginTop: -6}}>
+        <View style={{ marginLeft: 10, marginTop: -6 }}>
           <Picker
             selectedValue={selectedFilter}
-            style={{height: 50, width: 160}}
+            style={{ height: 50, width: 160 }}
             mode={"dialog"}
             onValueChange={(itemValue, itemIndex) => {
               setSelectedFilter(itemValue);
             }}
           >
-            
             <Picker.Item label="Courses" value="courses" />
             <Picker.Item label="Attended" value="attended" />
             <Picker.Item label="Missed" value="missed" />
@@ -90,34 +58,29 @@ const Attendance = () => {
           </Picker>
         </View>
       </View>
-      
-      {mockData.length !== 0 ? (
+
+      {attendanceData.length !== 0 ? (
         <FlatList
           keyExtractor={(item) => item.id}
-          data={mockData}
-          renderItem={({item}) => 
-            <AttendanceItem item={item}/>
-          }
+          data={attendanceData}
+          renderItem={({ item }) => <AttendanceItem item={item} />}
         />
-
-        ) : (
-        <View style={{marginLeft: 20}}>
-          <MaterialCommunityIcons name="cloud-sync-outline" size={26}/>
+      ) : (
+        <View style={{ marginLeft: 20 }}>
+          <MaterialCommunityIcons name="cloud-sync-outline" size={26} />
           <Text style={styles.font}>No data found!</Text>
         </View>
-        )}
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: 
-    {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    }
-    
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default Attendance;
