7f002157a275d6b42b143421b362b53cd4337706
fifi98 <bel.filip@hotmail.com>
2021-01-03 14:38:50 +0100

    Show error if non valid QR code has been scanned

--- mobile/screens/student/QR.js
+++ mobile/screens/student/QR.js
@@ -1,74 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import QRCodeScanner from "react-native-qrcode-scanner";
 import { Dimensions, View } from "react-native";
-import { Text } from "react-native-paper";
-import { showMessage, hideMessage } from "react-native-flash-message";
+import { showMessage } from "react-native-flash-message";
 
 import api from "../../utils/api";
 import { useSelector } from "react-redux";
 
 const QR = ({ navigation }) => {
-  const [attendanceSubmited, setAttendanceSubmited] = useState(null);
-
   const user = useSelector((state) => state.userId);
 
-  useEffect(() => {
-    // if (attendanceSubmited) {
-    //   showMessage({
-    //     message: "Thank you!",
-    //     description: "Your attendance has been saved!",
-    //     type: "success",
-    //     duration: 5000,
-    //     icon: "success",
-    //   });
-    //   setTimeout(() => {
-    //     navigation.pop();
-    //   }, 1500);
-    // }
-    // if(!attendanceSubmited){
-    //   showMessage({
-    //     message: "Error occured!",
-    //     description: "Please contact professor to submit your attendance manually!",
-    //     type: "danger",
-    //     duration: 5000,
-    //     icon: "danger"
-    //   });
-    //   setTimeout(() => {
-    //     navigation.pop();
-    //   }, 1500);
-    // }
-  });
+  const onScanned = async (e) => {
+    try {
+      const qrCodeData = JSON.parse(e.data);
+      await api.post("/attendance/mark", { ...qrCodeData, user });
 
-  const onScanned = (e) => {
-    const qrCodeData = JSON.parse(e.data);
-    console.log(qrCodeData);
-
-    console.log("USER ID", user);
-
-    api
-      .post("/attendance/mark", { ...qrCodeData, user })
-      .then(({ data }) => {
-        showMessage({
-          message: "Thank you!",
-          description: "Your attendance has been saved!",
-          type: "success",
-          duration: 5000,
-          icon: "success",
-        });
-      })
-      .catch((error) => {
-        showMessage({
-          message: "Error occured!",
-          description:
-            "You either scanned an invalid QR code or you have already marked your attendance to this lecture. If you think this is an error, please ask the teacher for assitance.",
-          type: "danger",
-          duration: 7000,
-          icon: "danger",
-        });
-      })
-      .finally(() => {
-        navigation.pop();
+      showMessage({
+        message: "Thank you!",
+        description: "Your attendance has been saved!",
+        type: "success",
+        duration: 5000,
+        icon: "success",
       });
+    } catch (error) {
+      showMessage({
+        message: "Error occured!",
+        description:
+          "You either scanned an invalid QR code or you have already marked your attendance to this lecture. If you think this is an error, please ask the teacher for assitance.",
+        type: "danger",
+        duration: 7000,
+        icon: "danger",
+      });
+    } finally {
+      navigation.pop();
+    }
   };
 
   return (
