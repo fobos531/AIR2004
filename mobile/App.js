7d0e2997b5627ebbfc60d0c5e30f84e99315be24
pgalina1 <pgalina@foi.hr>
2021-01-05 19:13:29 +0100

    Application theme change and minor tweaks on Attendance and Statistics screen

--- mobile/App.js
+++ mobile/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from "react-native-paper";
 import userReducer from "./reducers/user";
 import Navigation from "./navigation";
 import signInOutMiddleware from "./middleware/signInOut";
@@ -11,11 +11,19 @@ import FlashMessage from "react-native-flash-message";
 
 const store = createStore(userReducer, applyMiddleware(signInOutMiddleware));
 
+const AppTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "rgb(255, 255, 255)",
+  },
+};
+
 const App = () => {
   return (
     <Provider store={store}>
       <PaperProvider>
-        <NavigationContainer>
+        <NavigationContainer theme={AppTheme}>
           <Navigation />
         </NavigationContainer>
       </PaperProvider>
