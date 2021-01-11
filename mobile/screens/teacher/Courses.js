45e006328ff54336c5791ba8d015895e081a3d30
Jakov Glavina <cyber.shot.jakov@gmail.com>
2021-01-11 22:02:22 +0100

    Additional tweaks to teacher screens

--- mobile/screens/teacher/Courses.js
+++ mobile/screens/teacher/Courses.js
@@ -1,121 +1,137 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo, useRef } from "react";
 import { View, StyleSheet, FlatList } from "react-native";
-import { IconButton, Button, Text, Title, Chip, Card } from "react-native-paper";
-import FontAwesomeIcons from "react-native-vector-icons/FontAwesome5";
+import { IconButton, Text, Title, Card, Button } from "react-native-paper";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 import { useDispatch, useSelector } from "react-redux";
 import { FAB, Provider as PaperProvider } from "react-native-paper";
-import api from '../../utils/api';
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+
+import api from "../../utils/api";
+import BlankSpacer from "react-native-blank-spacer";
+import LectureItem from "./components/LectureItem";
 
 const Courses = ({ navigation }) => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state);
+  const user = useSelector((state) => state);
 
   const [courses, setCourses] = useState([]);
 
-  useEffect(() => { 
-    api.get('/user/details', {
-      headers: {
-        Authorization: `Bearer ${user.token}`,
-        "Content-Type": "application/json", 
-      }
+  console.log("USER", user);
+  const [lectures, SetLectures] = useState([]);
+  console.log("LECTURES", lectures);
+  useEffect(() => {
+    api
+      .get("/user/details", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
       })
       .then(({ data }) => setCourses(data.data.assignedCourses))
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
+    api
+      .get("/lecture/lecturesForTeacher", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then(({ data }) => SetLectures(data.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  // ref
+  const bottomSheetRef = useRef(null);
+
+  // variables
+  const snapPoints = useMemo(() => ["1%", "50%", "80%"], []);
+
+  // callbacks
+
+  const handleSheetChange = useCallback((index) => {
+    console.log("handleSheetChange", index);
   }, []);
 
   return (
-    <PaperProvider>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
         <Text style={styles.pageTitle}>Your courses</Text>
-        {courses.map(({ id, name }) => (
-          <Card style={styles.courseCard} key={id}>
+        <IconButton icon="plus" size={30} onPress={() => navigation.push("NewCourse")} />
+      </View>
+
+      {courses.map(({ id, name, passcode, allowedAbsences }) => (
+        <>
+          <Card key={id}>
             <Card.Content>
-              <Title style={styles.cardTitle}>{name}</Title>
+              <View style={{ flexDirection: "row", justifyContent: "flex-end", alignItems: "center" }}>
+                <Title style={styles.cardTitle}>{name}</Title>
+                <IconButton
+                  icon={() => <MaterialCommunityIcon name="pencil-outline" size={20} />}
+                  onPress={() => navigation.push("EditCourse")}
+                />
+                <IconButton
+                  icon={() => <MaterialCommunityIcon name="trash-can-outline" size={20} />}
+                  onPress={() => console.log("Potrebno implementirati brisanje")}
+                />
+              </View>
+
               <View style={styles.cardContentWrapper}>
-                <View style={styles.cardLectureTypes}>
-                  {/* {lectureTypes.map(lectureType => <Chip style={styles.lectureTypeBadge} textStyle={{ color: '#9b5cf4', fontWeight: 'bold' }}>{lectureType}</Chip>)} */}
-                  <Chip style={styles.lectureTypeBadge} textStyle={{ color: '#9b5cf4', fontWeight: 'bold' }}>Lecture</Chip>
-                  <Chip style={styles.lectureTypeBadge} textStyle={{ color: '#9b5cf4', fontWeight: 'bold' }}>Lab</Chip>
-                </View>
-                <View style={styles.cardActions}>
-                  <IconButton style={styles.courseIcon} icon={() => (<FontAwesomeIcons name="pen" size={16}/> )} onPress={() => navigation.push("EditCourse")} /> 
-                  <IconButton style={styles.courseIcon} icon={() => (<FontAwesomeIcons name="trash" size={16}/> )} onPress={() => console.log('Potrebno implementirati brisanje')} />
-                </View>
+                <Text>Allowed absences: {allowedAbsences}</Text>
+                <Text>Course passcode: {passcode}</Text>
+                <View style={styles.cardActions}></View>
               </View>
             </Card.Content>
           </Card>
-        ))}
-      </View>
-      <FAB
-        style={styles.fab}
-        small
-        label="add"
-        icon="plus"
-        color="black"
-        onPress={() => navigation.push("NewCourse")}
-      />
-    </PaperProvider>
+          <BlankSpacer height={15} />
+        </>
+      ))}
+      <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints} onChange={handleSheetChange}>
+        <Text style={{ fontWeight: "bold", fontSize: 19, paddingLeft: 15 }}>Recent lectures</Text>
+        <BottomSheetFlatList
+          keyExtractor={(lecture) => lecture.id}
+          data={lectures}
+          renderItem={({ item }) => <LectureItem lecture={item} />}
+        />
+      </BottomSheet>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: 
-    {
-        flex: 1,
-        margin: 25,
-    },
-    courseIcon:
-    {
-      backgroundColor: "white",
-      width: 35,
-    },
-    pageTitle:
-    {    
-      fontWeight: "bold",
-      fontSize: 24,
-      marginBottom: 15,
-    },
-    fab: 
-    {
-      position: 'absolute',
-      marginBottom: 40,
-      marginRight: 20,
-      right: 0,
-      bottom: 0
-    },
-    courseCard:
-    {
-      marginBottom: 10,
-    },
-    cardTitle:
-    {
-      fontSize: 18,
-      fontWeight: 'normal',
-    },
-    cardContentWrapper:
-    {
-      flexDirection: 'row',
-      flexWrap: 'wrap', 
-      alignItems: 'center',
-    },
-    cardLectureTypes:
-    {
-      flexDirection: 'row',
-    },
-    cardActions:
-    {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'flex-end',
-    },
-    lectureTypeBadge:
-    {
-      borderWidth: 1.5,
-      marginRight: 5,
-      borderColor: '#9b5cf4',
-      backgroundColor: '#f2eafe',
-      
-    },
+  container: {
+    flex: 1,
+    margin: 25,
+  },
+
+  pageTitle: {
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+
+  cardTitle: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "normal",
+  },
+  cardContentWrapper: {
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+  cardLectureTypes: {
+    flexDirection: "row",
+  },
+  cardActions: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  lectureTypeBadge: {
+    borderWidth: 1.5,
+    marginRight: 5,
+    borderColor: "#9b5cf4",
+    backgroundColor: "#f2eafe",
+  },
 });
 
 export default Courses;
